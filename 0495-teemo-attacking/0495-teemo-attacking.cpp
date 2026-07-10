4409f41fa160ada623cdc43c4ad6d58935579b56
Sahil Juneja <sahiljuneja7579@gmail.com>
2026-07-10 23:34:41 +0530

    Time: 0 ms (100%), Space: 29.6 MB (94.84%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.cpp
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
         int n=timeSeries.size();
+        if(n==0) return 0;
         int c=0;
         for(int i=0;i<n-1;i++){
             c+=min(duration,timeSeries[i+1]-timeSeries[i]);
